e4cc4d33e2752706fdf020a19359b11f1206bdb0
1391020381 <1391020381@qq.com>
2023-06-02 16:45:30 +0800

    feat: custom-provider

--- custom-provider/src/app.controller.ts
+++ custom-provider/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('app_service') private readonly appService2: AppService,
+    @Inject('person') private readonly person: { name: string; age: number },
+    @Inject('person2') private readonly person2: { name: string; desc: string },
+    @Inject('person5') private readonly person5: { name: string; desc: string },
+  ) {}
 
   @Get()
   getHello(): string {
+    console.log('appService2:', this.appService2.getHello());
+    console.log('person:', this.person);
+    console.log('person2:', this.person2);
+    console.log('person5:', this.person5);
     return this.appService.getHello();
   }
 }
--- custom-provider/src/app.module.ts
+++ custom-provider/src/app.module.ts
@@ -6,6 +6,40 @@ import { CatsModule } from './cats/cats.module';
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'app_service',
+      useClass: AppService,
+    },
+    {
+      provide: 'person',
+      useValue: {
+        name: 'aaaa',
+        age: 20,
+      },
+    },
+    {
+      provide: 'person2',
+      useFactory() {
+        return {
+          name: 'bbb',
+          desc: 'cccc',
+        };
+      },
+    },
+    {
+      provide: 'person5',
+      async useFactory() {
+        await new Promise((resolve) => {
+          setTimeout(resolve, 3000);
+        });
+        return {
+          name: 'bbb',
+          desc: 'cccc',
+        };
+      },
+    },
+  ],
 })
 export class AppModule {}
