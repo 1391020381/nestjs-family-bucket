5cacdd9dc15fadc6735b5b427dc0ca7385bc993b
1391020381 <1391020381@qq.com>
2023-09-13 15:24:17 +0800

    feat: 自定义装饰器和ExecutionContext：切换不同上下文

--- docs-nestjs/nestjs.md
+++ docs-nestjs/nestjs.md
@@ -10,3 +10,16 @@
 
 10. 方法装饰器
 11. 自定义参数装饰器
+
+# ExecutionContext：切换不同上下文
+
+## Exception Filter
+
+1. exception
+2. host: ArgumentsHost
+
+## Guard
+
+1. context: ExecutionContext
+
+- ExecutionContext 是 ArgumentHost 的子类 拓展了 getClass gethandler
--- docs-nestjs/src/app.controller.ts
+++ docs-nestjs/src/app.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get, SetMetadata, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  SetMetadata,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AaaGuard } from './aaa.guard';
 import { Aaa } from './aaa.decorator';
 import { Bbb } from './bbb.decorator';
 import { Ccc } from './ccc.decorator';
+import { DddGuard } from './ddd.guard';
+import { DddInterceptor } from './ddd.interceptor';
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
@@ -22,5 +30,12 @@ export class AppController {
   @Get('hello4')
   getHello4(@Ccc() c) {
     return c;
+  }
+  @Get('hello5')
+  @UseGuards(DddGuard)
+  @UseInterceptors(DddInterceptor)
+  @SetMetadata('roles', ['admin'])
+  getHello5(): string {
+    return 'getHello5';
   }
 }
