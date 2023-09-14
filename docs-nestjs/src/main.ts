9d5983eb31ff8e593e86947ee4bdff02e80e6cf7
1391020381 <1391020381@qq.com>
2023-09-14 18:40:51 +0800

    feat: upload

--- docs-nestjs/src/main.ts
+++ docs-nestjs/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { CustomExceptionFilter } from './common/CustomExceptionFilter';
+import { join } from 'path';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
   });
@@ -16,6 +18,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
   await app.listen(3000);
 }
 bootstrap();
