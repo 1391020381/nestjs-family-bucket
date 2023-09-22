8bec26942f89f15cb8759d73fd897bcf19447637
1391020381 <1391020381@qq.com>
2023-09-22 16:12:44 +0800

    feat: User

--- meeting_room_booking_system_backend/src/user/user.controller.ts
+++ meeting_room_booking_system_backend/src/user/user.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Post('register')
+  register(@Body() registerUser: RegisterUserDto) {
+    console.log(registerUser);
+    return registerUser;
+  }
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
--- meeting_room_booking_system_backend/src/user/user.service.ts
+++ meeting_room_booking_system_backend/src/user/user.service.ts
@@ -1,9 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { RegisterUserDto } from './dto/register-user.dto';
 @Injectable()
 export class UserService {
+  private logger = new Logger();
+
+  @InjectRepository(User)
+  private userRepository: Repository<User>;
+
+  async register(user: RegisterUserDto) {
+    return user;
+  }
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
