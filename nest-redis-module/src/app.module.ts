65fbe3dbbdb1e715326df9654b3d279c57961776
1391020381 <1391020381@qq.com>
2023-09-26 18:29:14 +0800

    nest-redis-module

--- nest-redis-module/src/app.module.ts
+++ nest-redis-module/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RedisModule } from './redis/redis.module';
+import { UserModule } from './user/user.module';
+import { GoodsModule } from './goods/goods.module';
 
 @Module({
-  imports: [RedisModule],
+  imports: [RedisModule, UserModule, GoodsModule],
   controllers: [AppController],
   providers: [AppService],
 })
