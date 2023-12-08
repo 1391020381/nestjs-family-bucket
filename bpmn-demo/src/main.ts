4f8daed3c4ae5ba05a8364107a79987a8d2988f3
p_jinzjzhou <1391020381@qq.com>
2023-12-08 11:19:56 +0800

    feat: bpmn panel

--- bpmn-demo/src/main.ts
+++ bpmn-demo/src/main.ts
@@ -7,4 +7,7 @@ import "bpmn-js/dist/assets/bpmn-js.css";
 import "bpmn-js/dist/assets/diagram-js.css";
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn.css";
 
+// 右边工具栏样式
+import "bpmn-js-properties-panel/dist/assets/properties-panel.css";
+import "bpmn-js-properties-panel/dist/assets/element-templates.css";
 createApp(App).use(router).mount("#app");
--- bpmn-demo/src/pages/basic.vue
+++ bpmn-demo/src/pages/basic.vue
@@ -1,23 +1,36 @@
 <template>
     <div class="containers">
         <div class="canvas" ref="canvas" id="canvas"></div>
+        <div class="panel" id="properties" ref="properties"></div>
     </div>
 </template>
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue'
 import BpmnModeler from 'bpmn-js/lib/Modeler'
+import {
+    BpmnPropertiesPanelModule,
+    BpmnPropertiesProviderModule,
+} from 'bpmn-js-properties-panel';
+
 import xmlStr from '../mock/xmlStr';
-let bpmnModeler = ref(null)
+let bpmnModeler: any = ref(null)
 let container = ref('')
 let canvas = ref(null)
-
+let properties = ref(null)
 onMounted(() => {
     init()
 })
 const init = () => {
     (bpmnModeler as any) = new BpmnModeler({
         // container: document.getElementById('canvas') || '' // canvas as any
-        container: canvas.value as any
+        container: canvas.value as any,
+        propertiesPanel: {
+            parent: "#properties"
+        },
+        additionalModules: [
+            BpmnPropertiesPanelModule,
+            BpmnPropertiesProviderModule
+        ]
     })
     createNewDiagram()
 }
