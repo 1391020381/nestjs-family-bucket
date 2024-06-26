ceb1af8289805e893c716d1d0219efa18bec2eb8
1391020381 <1391020381@qq.com>
2024-06-26 14:07:26 +0800

    feat: all-decoraror add

--- all-decorator/src/app.controller.ts
+++ all-decorator/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Optional } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import { Guang } from './guang';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly guangUseClassProvide: Guang,
+  ) {}
+
+  @Inject('Guang')
+  private readonly guang: Record<string, any>;
+
+  // 可选
+  @Optional()
+  @Inject('Guang1')
+  private readonly guang1: Record<string, any>;
 
   @Get()
   getHello(): string {
+    console.log('Guang:', this.guang);
+    console.log('guangUseClassProvide:', this.guangUseClassProvide.getHello());
     return this.appService.getHello();
   }
 }
--- all-decorator/src/app.module.ts
+++ all-decorator/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { Guang } from './guang';
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    Guang,
+    {
+      provide: 'Guang',
+      useFactory() {
+        return {
+          name: 'guang',
+        };
+      },
+    },
+  ],
 })
 export class AppModule {}
